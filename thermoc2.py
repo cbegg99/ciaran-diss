02c1916bf1a9dd50cf87b56f15e4852642ad4f41
cbegg99 <beggciaran99@gmail.com>
2022-04-02 15:20:58 +0100

    Globals integrated into thermoc
    Writing to different files so globals has to be customised and entered into each thermoc (?)

--- thermoc2.py
+++ thermoc2.py
@@ -1,28 +1,30 @@
+# Import modules
 import Adafruit_MAX31855.MAX31855 as MAX31855
-from globals import runMeasurements
+from time import sleep, time
 
-# Raspberry Pi software SPI configuration.
+# Raspberry Pi configuration.
 CLK = 13
 CS  = 19
 DO  = 26
 sensor = MAX31855.MAX31855(CLK, CS, DO)
 
-from time import sleep, time
+# Define file path to save
+file_path1 = "/home/pi/Ciaran/data_files/test2.csv"
 
 # Function to run the infinite while loop 
-def runMeasurements(sensor):
-    i=0
+i=0
 
-    # Loop printing measurements every second.
-    print('Press Ctrl-C to quit.')
 
-    while True:
-        temp = sensor.readTempC()
-        print('Thermocouple 2 Temperature: {0:0.3F}*C ')
-        sleep(1.0)
-        i+=1
+print('Press Ctrl-C to quit.')
 
-        file = open("/home/pi/Ciaran/data_files/test2.csv", "a")
-        seconds=time()
-        file.write(str(seconds) + "," + str(temp) + "\n")
-        file.close()
+# Loop printing measurements every __ seconds.
+while True:
+    temp = sensor.readTempC()
+    print('Thermocouple 2 Temperature: {0:0.3F}*C ')
+    sleep(1.0)
+    i+=1
+
+    file = open(file_path1, "x")
+    seconds=time()
+    file.write(str(seconds) + "," + str(temp) + "\n")
+    file.close()
